c4c20024719732645c20f2367cfd0dd8d6ab2119
Manuellama <manuellamapaniagua@yahoo.es>
2016-08-05 13:23:34 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+DEBUG="debug"
+#DEBUG=""
+GCC_PATH=`pwd`/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin
+export PATH=$GCC_PATH:$PATH
+export USE_CCACHE=1
+export CCACHE_DIR=`pwd`/.ccache
+prebuilts/misc/linux-x86/ccache/ccache -M 120G
+# Fix build dependency
+mkdir -p prebuilts/qemu-kernel/arm
+touch prebuilts/qemu-kernel/arm/LINUX_KERNEL_COPYING
+source build/envsetup.sh
+lunch 8
+make -j16 otapackage
