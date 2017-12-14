c3afa21f99d0d6a9dae63ba5990a778d94b8a707
Manuel Lama Paniagua <manuellamapaniagua@yahoo.es>
2017-12-14 14:52:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+DEBUG="debug"
+#DEBUG=""
+
+GCC_PATH=`pwd`/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin
+export PATH=$GCC_PATH:$PATH
+
 export USE_CCACHE=1
 export CCACHE_DIR=`pwd`/.ccache
 prebuilts/misc/linux-x86/ccache/ccache -M 120G
+
 # Fix build dependency
 mkdir -p prebuilts/qemu-kernel/arm
 touch prebuilts/qemu-kernel/arm/LINUX_KERNEL_COPYING
 
 source build/envsetup.sh
-brunch wingray
+lunch aosp_wingray-user$DEBUG
+make -j 16 otapackage 2>&1 | tee build.log
